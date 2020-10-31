ee7823f5946bee6fbc579113f409734153488967
jw <sunrinint@gmail.com>
2020-11-01 02:21:47 +0900

    1101 아 힘들어 죽겠다

--- app/src/main/java/app/sunrin/codegreen/AfterScanActivity.java
+++ app/src/main/java/app/sunrin/codegreen/AfterScanActivity.java
@@ -1,25 +1,160 @@
 package app.sunrin.codegreen;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class AfterScanActivity extends AppCompatActivity {
+    Context context;
+    TextView textView, textView2, textView3;
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            initView();
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+            Toast.makeText(AfterScanActivity.this, "권한 허용을 하지 않으면 서비스를 이용할 수 없습니다.", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_scan);
         SharedPreferences preferences = getSharedPreferences("BarcodeResult", 0);
         String result = preferences.getString("result", "");
-        System.out.println("■");
-        System.out.println(result);
-        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
         Button button = findViewById(R.id.button);
         button.setText(result);
+
+        context = AfterScanActivity.this;
+
+        // 네트워크 연결상태 체크
+        if (NetworkConnection() == false) NotConnected_showAlert();
+        checkPermissions();
+
+
     }
+
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) { // 마시멜로(안드로이드 6.0) 이상 권한 체크
+            TedPermission.with(context)
+                    .setPermissionListener(permissionlistener)
+                    .setRationaleMessage("앱을 이용하기 위해서는 접근 권한이 필요합니다")
+                    .setDeniedMessage("앱에서 요구하는 권한설정이 필요합니다...\n [설정] > [권한] 에서 사용으로 활성화해주세요.")
+                    .setPermissions(new String[]{
+                            android.Manifest.permission.WRITE_CONTACTS, // 주소록 액세스 권한
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE // 기기, 사진, 미디어, 파일 엑세스 권한
+                    })
+                    .check();
+
+        } else {
+            initView();
+        }
+    }
+
+
+    private void initView() {
+
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+
+       // new getCategory().execute();
+        new getProductName().execute();
+        //new getImage().execute();
+    }
+
+
+    private class getProductName extends AsyncTask<String, Void, String> {
+        // String 으로 값을 전달받은 값을 처리하고, Boolean 으로 doInBackground 결과를 넘겨준다.
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Connection.Response response = Jsoup.connect("http://gs1.koreannet.or.kr/pr/8808244201014").method(Connection.Method.GET).execute();
+                Document document = response.parse();
+                Element category = document.select("tr[class=b_line]").first();
+                System.out.println(category);
+                String categoryNew = category.toString();
+                String categoryNew2 = categoryNew.replace("<tr class=\"b_line\">", "").replace("<th>KAN 상품분류</th>", "").replace("&gt;", ">").replace("</tr>", "").replace("<td>", "").replace("</td", "");
+
+                return categoryNew2;
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            System.out.println(result);
+            textView2.setText(result);
+        }
+    }
+
+    private void NotConnected_showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(AfterScanActivity.this);
+        builder.setTitle("네트워크 연결 오류");
+        builder.setMessage("사용 가능한 무선네트워크가 없습니다.\n" + "먼저 무선네트워크 연결상태를 확인해 주세요.")
+                .setCancelable(false)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish(); // exit
+                        //application 프로세스를 강제 종료
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+    private boolean NetworkConnection() {
+        int[] networkTypes = {ConnectivityManager.TYPE_MOBILE, ConnectivityManager.TYPE_WIFI};
+        try {
+            ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            for (int networkType : networkTypes) {
+                NetworkInfo activeNetwork = manager.getActiveNetworkInfo();
+                if (activeNetwork != null && activeNetwork.getType() == networkType) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return false;
+    }
+
+
 }
