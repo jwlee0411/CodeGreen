73eef86016e71d74558babf11b6ef68c655ceb05
jw <sunrinint@gmail.com>
2020-11-05 18:16:43 +0900

    1105 Final

--- app/src/main/java/app/sunrin/codegreen/AfterScanActivity.java
+++ app/src/main/java/app/sunrin/codegreen/AfterScanActivity.java
@@ -12,13 +12,24 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,9 +46,11 @@ import java.util.HashMap;
 
 public class AfterScanActivity extends AppCompatActivity {
 
-    TextView productName, productCategory, textView3;
+    String[] check_how;
+    TextView productName, productCategory,textView3;
     String url, Shorturl;
-    String Category;
+    String Category, db_category, KANcode;
+    Boolean prodName;
     Bitmap bitmap;
     ImageView img;
     String result;
@@ -47,37 +60,42 @@ public class AfterScanActivity extends AppCompatActivity {
 
     SharedPreferences preferences, preferences1;
     // data arraylist에 데이터를 넣는 함수
+    //플라스틱 1,종이 2,비닐 3,캔 4,스티로폼 5,페트병 6,유리 7,일반쓰레기 8
     public void addPlastic()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"플라스틱","플라스틱은 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.pp),"플라스틱","플라스틱의 이물질을 제거 후 버려주세요"));
     }
 
     public void addPaper()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"종이","종이는 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.paper),"종이","종이에 붙어 있는 테이프 등 종이가 아닌 재질을 제거후 버려주세요"));
 
     }
     public void addVinyl()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"비닐","비닐은 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.vinyl),"비닐","비닐의 이물질을 제거 후 버려주세요"));
 
     }
     public void addCan()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"캔","캔은 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.can),"캔","캔의 내용물을 제거 후 버려주세요"));
 
     }
     public void addStrph()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"스티로폼","스티로폼은 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.ps),"스티로폼","스티로폼의 이물질을 제거후 이물질이 없는 상태로 버려주세요"));
     }
     public void addPet()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"페트병","페트병은 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.pete),"페트병","페트병 안의 내용물을 제거한 뒤 페트병에 붙어 있는 비닐과 뚜껑을 제거 후 분리수거 합니다."));
     }
     public void addTrash()
     {
-        data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"일반쓰레기","일반쓰레기는 말이여,,,"));
+        data.add(addItem(getResources().getDrawable(R.drawable.normal_black),"일반쓰레기","종량제 봉투에 담어 버려주세요."));
+    }
+    public void addGlass()
+    {
+        data.add(addItem(getResources().getDrawable(R.drawable.glass),"유리","공병의 경우 이물질을 제거 후 버려주세요"));
     }
 
     @Override
@@ -111,6 +129,7 @@ public class AfterScanActivity extends AppCompatActivity {
 
 
 
+
     }
 
     public Item addItem(Drawable img, String title, String way)
@@ -131,47 +150,92 @@ public class AfterScanActivity extends AppCompatActivity {
         new getName().execute();//상품명 가져오기
         new getCategory().execute();//상품 카테고리 가져오기
         new getPhoto1().execute();
+        new getKANcode().execute();
+
 
 
 
 
         System.out.println(Category);
-        switch(result) // 1차로 바코드 번호를 불러와서 분리수거 정보를 가져온다.
-        {
-            case "8808244201014":
-                addVinyl();
-                break;
-            case "8809482500358":
-               addPet();
-                break;
-            default: //등록된 바코드가 없을 경우
+//        switch(KANcode) // 1차로 바코드 번호를 불러와서 분리수거 정보를 가져온다.
+//        {
+//            case "01010101":
+//                addVinyl();
+//                break;
+//            case "01010102":
+//                addPet();
+//                break;
+//            case "01160304":
+//
+//                break;
+//
+//
+//            default: //등록된 바코드가 없을 경우
+///*
+//                if(Category.contains("생수")) //카테고리를 기준으로 상품을 구분한다.
+//                {
+//                    data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"페트병","페트병은 말이여,,,"));
+//                }
+//                else //해당되는 카테고리도 없을 경우
+//                {
+//                */
+//
+//                    //분리수거에 관한 모든 정보를 띄워준다.
+//                    addCan();
+//                    addPaper();
+//                    addPet();
+//                    addPlastic();
+//                    addStrph();
+//                    addTrash();
+//                    addGlass();
+//                    addVinyl();
+//               // }
+//
+//        }
 /*
-                if(Category.contains("생수")) //카테고리를 기준으로 상품을 구분한다.
-                {
-                    data.add(addItem(getResources().getDrawable(R.drawable.ic_launcher_foreground),"페트병","페트병은 말이여,,,"));
+        if (KANcode==null){
+            addCan();
+            addPaper();
+            addPet();
+            addPlastic();
+            addStrph();
+            addTrash();
+            addGlass();
+            addVinyl();
+        }else{
+            for(int i=0; i<check_how.length; i++){
+                switch (""+i){
+                    case "1":
+                        addPlastic();
+                        break;
+                    case "2":
+                        addPaper();
+                        break;
+                    case "3":
+                        addVinyl();
+                        break;
+                    case "4":
+                        addCan();
+                        break;
+                    case "5":
+                        addStrph();
+                        break;
+                    case "6":
+                        addPet();
+                        break;
+                    case "7":
+                        addGlass();
+                        break;
+                    case "8":
+                        addTrash();
+                        break;
+
                 }
-                else //해당되는 카테고리도 없을 경우
-                {
-                */
-
-                    //분리수거에 관한 모든 정보를 띄워준다.
-                    addCan();
-                    addPaper();
-                    addPet();
-                    addPlastic();
-                    addStrph();
-                    addTrash();
-
-               // }
-
+            }
         }
+*/
 
 
-
-        recyclerView =findViewById(R.id.recycling);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ReAdapter reAdapter = new ReAdapter(data);
-        recyclerView.setAdapter(reAdapter);
 
     }
 
@@ -205,9 +269,65 @@ public class AfterScanActivity extends AppCompatActivity {
         {// 결과값을 화면에 표시함.
 
             productName.setText(result);
+            System.out.println("★" + result);
             Category = result;
+            if(result=="")
+            {
+                prodName = false;
+                Toast.makeText(getApplicationContext(), "상품 정보가 없습니다.", Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                prodName = true;
+            }
+
         }
     }
+
+    private class getKANcode extends AsyncTask<String, Void, String> //KANcode 가져오기
+    {
+
+        @Override
+        protected String doInBackground(String... params)
+        {
+            try {
+                Connection.Response response = Jsoup.connect(url).method(Connection.Method.GET).execute();
+                Document document = response.parse();
+
+                Element product = document.select("tr").first();
+                System.out.println(product);
+                String productNew = product.toString();
+                String productNew2 = productNew.replace("<tr>", "").replace("<th>KAN 상품분류코드</th>", "").replace("&gt;", ">").replace("</tr>", "").replace("<td>", "").replace("</td>", "");
+                return productNew2;
+
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {//kancode 저장
+
+            if(prodName == false)
+            {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                KANcode=result.substring(7, 14);
+                System.out.println("KAN : " + KANcode);
+
+                loadDb();
+            }
+
+        }
+    }
+
 
     private class getCategory extends AsyncTask<String, Void, String> //상품 카테고리 가져오기
     {
@@ -224,7 +344,7 @@ public class AfterScanActivity extends AppCompatActivity {
                     Element product = document.select("tr[class=b_line]").first();
                     System.out.println(product);
                     String productNew = product.toString();
-                    String productNew2 = productNew.replace("<tr class=\"b_line\">", "").replace("<th>KAN 상품분류</th>", "").replace("&gt;", ">").replace("</tr>", "").replace("<td>", "").replace("</td", "");
+                    String productNew2 = productNew.replace("<tr class=\"b_line\">", "").replace("<th>KAN 상품분류</th>", "").replace("&gt;", ">").replace("</tr>", "").replace("<td>", "").replace("</td>", "");
                     return productNew2;
 
 
@@ -240,6 +360,7 @@ public class AfterScanActivity extends AppCompatActivity {
         {// 결과값을 화면에 표시함.
 
                 productCategory.setText(result);
+                System.out.println(result);
         }
     }
 
@@ -367,7 +488,88 @@ public class AfterScanActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void loadDb(){ //파베에서 KAN코드에 대한 카테고리 가져옴
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        System.out.println(KANcode);
+        DatabaseReference db_KANcode = database.getReference(KANcode);
 
+        // Read from the database
+        db_KANcode.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                db_category = dataSnapshot.getValue(String.class);
+
+
+                System.out.println("dbcat : "+db_category);
+                if(db_category==null)
+                {
+                    //분리수거에 관한 모든 정보를 띄워준다.
+                    addCan();
+                  addPaper();
+                addPet();
+             addPlastic();
+                addStrph();
+              addTrash();
+                addGlass();
+                addVinyl();
+                }
+                else
+                {
+                    check_how = db_category.split(",");
+
+                    for(int i = 0; i<check_how.length; i++)
+                    {
+                        System.out.println(check_how[i]);
+                        switch (check_how[i])
+                        {
+
+                            //플라스틱 1,종이 2,비닐 3,캔 4,스티로폼 5,페트병 6,유리 7,일반쓰레기 8
+                            case "1": addPlastic(); break;
+                            case "2": addPaper(); break;
+                            case "3" : addVinyl(); break;
+                            case "4" : addCan(); break;
+                            case "5" : addStrph(); break;
+                            case "6" : addPet(); break;
+                            case "7" : addGlass(); break;
+                            case "8" : addTrash(); break;
+
+
+                        }
+                    }
+                }
+
+
+                recyclerView =findViewById(R.id.recycling);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                ReAdapter reAdapter = new ReAdapter(data);
+                recyclerView.setAdapter(reAdapter);
+
+                Button button = findViewById(R.id.button);
+                button.setVisibility(View.VISIBLE);
+                button.setOnClickListener(v->{
+                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                    sharingIntent.setType("text/plain");		// 고정 text
+                    sharingIntent.putExtra(Intent.EXTRA_TEXT, productName.getText().toString() + productCategory.getText().toString());
+                   // sharingIntent.setPackage("com.kakao.talk");	// 고정 text
+                    startActivity(sharingIntent);
+
+                });
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+    }
 
 
 
--- app/src/main/java/app/sunrin/codegreen/MainActivity.java
+++ app/src/main/java/app/sunrin/codegreen/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.util.Scanner;
 import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,18 +40,86 @@ public class MainActivity extends AppCompatActivity {
             IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
             String re = scanResult.getContents();
 
-            Log.d("onActivityResult", "onActivityResult: ." + re);
 
-            //Toast.makeText(this, re, Toast.LENGTH_LONG).show(); // 바코드에 인식된 숫자 Toast로 띄우기
+            char tmp;
+            boolean output = true;	// 결과값을 저장할 변수, 참/거짓밖에 없기 때문에 boolean으로 선언
 
-            SharedPreferences preferences = getSharedPreferences("BarcodeResult", 0);
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putString("result", re);
-            editor.commit();
+            for(int i = 0 ; i < re.length() ; i++) {
+                tmp = re.charAt(i);	//한글자씩 검사하기 위해서 char형 변수인 tmp에 임시저장
+                if(!('0' <= tmp &&  tmp <= '9')) {	//문자가 0 ~ 9 사이가 아닐경우
+                    output = false;	//output을 false로 바꾼다.
+                }
+            }
 
-            Intent intent1 = new Intent(getApplicationContext(), AfterScanActivity.class);
-            startActivity(intent1); //새 창(상품정보 띄울 창) 띄우기
-            finish();
+            if(output)
+            {
+                String[] resultSplit = re.split("");
+                int[] resultInt = new int[100];
+                int oddNum = 0;
+                int evenNum = 0;
+
+
+
+                for(int i = 0; i<resultSplit.length-1; i++)
+                {
+                    resultInt[i] = Integer.parseInt(resultSplit[i]);
+
+                    if(i%2!=0) //짝수이면
+                    {
+                        evenNum = evenNum + resultInt[i];
+                        System.out.println("※" + resultInt[i]);
+                    }
+                    else
+                    {
+                        oddNum = oddNum + resultInt[i];
+                        System.out.println("★" + resultInt[i]);
+                    }
+                }
+                resultInt[resultSplit.length-1] = Integer.parseInt(resultSplit[resultSplit.length-1]);
+                System.out.println(oddNum);
+                System.out.println(evenNum);
+
+                System.out.println(resultSplit.length);
+                System.out.println(resultInt[resultSplit.length-1]);
+
+
+                int checkDigit = 10 - (oddNum + evenNum*3)%10;
+                if(checkDigit==10)
+                {
+                    checkDigit = 0;
+                }
+                if(resultSplit.length>=11 && (resultInt[resultSplit.length-1] == checkDigit)) //체크숫자가 맞으면
+                {
+                    Log.d("onActivityResult", "onActivityResult: ." + re);
+
+                    //Toast.makeText(this, re, Toast.LENGTH_LONG).show(); // 바코드에 인식된 숫자 Toast로 띄우기
+
+
+                    SharedPreferences preferences = getSharedPreferences("BarcodeResult", 0);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("result", re);
+                    editor.commit();
+
+                    Intent intent1 = new Intent(getApplicationContext(), AfterScanActivity.class);
+                    startActivity(intent1); //새 창(상품정보 띄울 창) 띄우기
+                    finish();
+                }
+                else //체크숫자가 틀리면
+                {
+                    Toast.makeText(getApplicationContext(), "올바른 바코드가 아닙니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "올바른 바코드가 아닙니다.", Toast.LENGTH_SHORT).show();
+            }
+
+
+
+
+
+
+
         }
     }
 }
